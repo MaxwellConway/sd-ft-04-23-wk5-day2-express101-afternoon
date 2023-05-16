0821c52628c13c18c8fb02e2ec37aa4ac40db137
MaxwellConway <maxconway23@gmail.com>
2023-05-16 16:21:01 -0400

    learning express, http requests

--- index.js
+++ index.js
@@ -3,9 +3,60 @@ const app = express();
 app.use(express.json());
 
 // Create an empty array to store restaurant data
-const restaurants = [];
+const restaurants = [{ name: "Uchi", style: "Asian", price: "$$$$" }];
 
 // Routes go here
+
+//add restaurant
+app.post("/add_restaurant", (req, res) => {
+  const newRestaurant = req.body;
+  restaurants.push(newRestaurant);
+  res.send(restaurants);
+
+  console.log(restaurants);
+});
+
+//update restaurant
+app.put("/update_restaurant", (req, res) => {
+  const restaurantUpdate = req.body;
+  for (let i = 0; i < restaurants.length; i++) {
+    if (restaurantUpdate.name === restaurants[i].name) {
+      restaurants.splice(i, 1, restaurantUpdate);
+    } else {
+    }
+  }
+  res.send(restaurants);
+  console.log(restaurants);
+});
+
+//delete restaurant
+app.post("/delete_restaurant", (req, res) => {
+  const filteredRestaurants = restaurants.filter(
+    (resta) => resta.name !== req.body.name
+  );
+  restaurants = filteredRestaurants;
+  res.send(restaurants);
+  //const deletedRestaurant = req.body;
+  //for (i = 0, i < restaurants.length; i++; ) {
+  //  if (deletedRestaurant.name === restaurants[i].name) {
+  //    restaurants.splice(i, 1);
+  //  } else {
+  //  }
+  //}
+  //res.send(restaurants);
+  //console.log(restaurants);
+});
+
+//list restaurant
+app.get("/list_restaurants", (req, res) => {
+  res.send(restaurants);
+
+  let counter = 0;
+  while (counter < restaurants.length) {
+    console.log(restaurants[counter].name);
+    counter++;
+  }
+});
 
 // Start the server
 app.listen(3000, () => {
